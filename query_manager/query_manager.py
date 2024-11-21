bca5673ce6f31f03a9f90c5fc36ea8a315401aec
Michael Madera <124817262+viewtifultiger@users.noreply.github.com>
2024-11-21 05:19:56 -0800

    Adding query_manager

---
+++ query_manager/query_manager.py
@@ -0,0 +1,44 @@
+import ollama
+
+class query_manager:
+    def __init__(self, embedding_model='nomic-embed-text'):
+        self.embedding_model = embedding_model
+    
+    def get_user_prompt(self):
+        return input("Send a message: ")
+    
+    def embed_user_prompt(self, prompt):
+        embedding = ollama.embed(model=self.embedding_model, input=f"search_query: {prompt}")
+        return embedding['embeddings']
+    
+    def query_database(self, conn, embedding):
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT chunk_vector
+                FROM document_data.chunks
+                ORDER BY 1 - (chunk_vector <=> %s::vector) DESC
+                LIMIT 1;
+                """,
+                (embedding,),
+            )
+            result = cur.fetchone()
+            return result
+    
+    def get_document_chunk(self, conn, embedding):
+        db_results = self.query_database(conn, embedding)
+        chunk_vector = db_results[0] 
+        
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT document_id, chunk_text 
+                FROM document_data.chunks 
+                WHERE chunk_vector = %s;
+                """,
+                (chunk_vector,),  
+            )
+            result = cur.fetchone()
+            dictionary = {'document_id': result[0], 'chunk_text': result[1]}
+            return dictionary
+           
