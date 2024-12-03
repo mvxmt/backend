48bf45e880ebc5d9ec50bdb984784d896581253b
Michael Madera <124817262+viewtifultiger@users.noreply.github.com>
2024-12-03 11:35:25 -0800

    Update query_manager.py
    updated functions: get_user_prompt, embed_user_prmpt, get_document_chunk

--- query_manager/query_manager.py
+++ query_manager/query_manager.py
@@ -1,44 +1,47 @@
 import ollama
+import numpy as np
 
-class query_manager:
+class QueryManager:
     def __init__(self, embedding_model='nomic-embed-text'):
         self.embedding_model = embedding_model
+        self.user_input = ""
     
-    def get_user_prompt(self):
-        return input("Send a message: ")
+
+    def get_user_prompt(self, user_input):
+        self.user_input = user_input
+        return self.user_input
     
-    def embed_user_prompt(self, prompt):
-        embedding = ollama.embed(model=self.embedding_model, input=f"search_query: {prompt}")
-        return embedding['embeddings']
+
+    def embed_user_prompt(self):
+        embedding = ollama.embed(model=self.embedding_model, input=f"search_query: {self.user_input}")
+        embedding_array = np.array(embedding['embeddings'])
+        return embedding_array
     
     def query_database(self, conn, embedding):
         with conn.cursor() as cur:
             cur.execute(
                 """
-                SELECT chunk_vector
+                SELECT * 
                 FROM document_data.chunks
                 ORDER BY 1 - (chunk_vector <=> %s::vector) DESC
-                LIMIT 1;
+                LIMIT 5;
                 """,
-                (embedding,),
+                (embedding.tolist(),), # Converts NumPy array to list for SQL
             )
-            result = cur.fetchone()
+            result = cur.fetchall()
             return result
     
-    def get_document_chunk(self, conn, embedding):
-        db_results = self.query_database(conn, embedding)
-        chunk_vector = db_results[0] 
-        
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT document_id, chunk_text 
-                FROM document_data.chunks 
-                WHERE chunk_vector = %s;
-                """,
-                (chunk_vector,),  
-            )
-            result = cur.fetchone()
-            dictionary = {'document_id': result[0], 'chunk_text': result[1]}
-            return dictionary
-           
+
+    async def get_document_chunk(self, conn, result):
+        # result is a list of tuples
+
+        # each tuple contains
+        """
+        index 0: id
+        index 1: document_id
+        index 2: chunk_text
+        index 3: chunk_vector
+        """
+
+        chunk_list = [{"document_id": row[1], "chunk_text": row[2]} for row in result]
+        return chunk_list
