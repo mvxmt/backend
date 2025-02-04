5b328edcb25dbe5751372b31550829c0f6e0bac3
viewtifultiger <viewtifultiger@gmail.com>
2025-02-04 05:23:28 -0800

    feat: parser uses unstructured

--- services/parser.py
+++ services/parser.py
@@ -1,4 +1,9 @@
 import os
+from unstructured.partition.text import partition_text
+from unstructured.partition.pdf import partition_pdf
+from unstructured.partition.doc import partition_doc
+from unstructured.partition.docx import partition_docx
+from unstructured.partition.xml import partition_xml
 
 class Parser:
     """
@@ -27,27 +32,39 @@ class Parser:
                 pass
         return doc
 
-    def get_document_chunks(self, doc: str, chunk_size: int):
+    def get_document_chunks(self, path: str, chunk_size: int):
         """
-        Takes in a doc (str) and chunk size (int), returns a list of text chunks
-        of the specified size. It replaces newlines with spaces to avoid unncessary 
-        line breaks. Emphasizes complete sentences to retain context over exact chunk size. 
+        Takes in a filepath (str) and returns chunk elements.
+
+        max_characters: int (default=500) The hard maximum size for a chunk. No chunk will 
+            exceed this number of characters. A single element that by itself exceeds this 
+            size will be divided into two or more chunks using text-splitting.
+        new_after_n_chars: int (default=max_characters) The "soft" maximum size for a chunk.
+            A chunk that already exceeds this number of characters will not be extended, even
+            if the next element would fit without exceeding the specified hard maximum. This
+            can be used in conjunction with max_characters to set a "preferred" size, like
+            "I prefer chunks of around 1000 chars, but I'd rather have a chunk of 1500
+            (max_characters) than resort to text-splitting" 
         """
-        size = chunk_size
-        chunks = []
-        current_chunk = []
-        text = doc.replace("\n", " ") #Replaces newlines with spaces to avoid unncessary line breaks
-        j = 0
-        for i in range(len(text)):
-            current_chunk.append(text[i]) #Add letter to current chunk
-            j += 1 #Additonal Counter to keep track of chunk size
-            #Continue the loop until the end of sentence is reached
-            if j == size:
-                if text[i] == ".":
-                    current_chunk = "".join(current_chunk)
-                    chunks.append(current_chunk)
-                    current_chunk = []
-                    j = 0
-                else:
-                    j -= 1
+        if not os.path.exists(path):
+            raise FileNotFoundError("File not found")
+        
+        _, extension = os.path.splitext(path)
+        match extension[1:]:
+            case "txt":
+                chunks = partition_text(filename=path, strategy="fast", chunking_strategy="basic", max_character = 200, new_after_n_chars = chunk_size)
+            case "pdf":
+                chunks = partition_pdf(filename=path, strategy="fast", chunking_strategy="basic", max_character = 200, new_after_n_chars = chunk_size)
+            case "doc":
+                """
+                .doc requires libreoffice to convert the file into a .docx before parsing,
+                dont forget to install libreoffice on server
+                """
+                chunks = partition_doc(filename=path, strategy="fast", chunking_strategy="basic", max_character = 200, new_after_n_chars = chunk_size)
+            case "docx":
+                chunks = partition_docx(filename=path, strategy="fast", chunking_strategy="basic", max_character = 200, new_after_n_chars = chunk_size)
+            case "xml":
+                chunks = partition_xml(filename=path, strategy="fast", chunking_strategy="basic", xml_keep_tags=True, max_character = 200, new_after_n_chars = chunk_size)
+            case _:
+                raise ValueError("Unsupported file type")
         return chunks
