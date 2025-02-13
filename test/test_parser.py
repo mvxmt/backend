75a68f9c766967037b8021ce455e42ac57756d41
viewtifultiger <viewtifultiger@gmail.com>
2025-02-12 20:10:00 -0800

    chore: test functions

---
+++ test/test_parser.py
@@ -0,0 +1,33 @@
+import unittest
+from parser import Parser
+
+class Test_Parser(unittest.TestCase):
+	def test_get_path_is_string(self):
+		pass
+	def test_get_document_returns_tuple(self):
+		pass
+	def test_get_document_first_index_is_string(self):
+		pass
+	def test_get_document_second_index_is_list(self):
+		pass
+	def test_get_text(self):
+		pass
+	def test_get_pdf(self):
+		pass
+	def test_get_doc(self):
+		pass
+	def test_get_docx(self):
+		pass
+	def test_get_xml(self):
+		pass
+	def test_get_chunks_doc_is_string(self):
+		pass
+	def test_get_chunks_chunk_size_is_int(self):
+		pass
+	def test_get_chunks_is_list(self):
+		pass
+	def test_get_chunks_is_list_of_strings(self):
+		pass
+	def test_get_chunks_max_tokens_matches_argument(self):
+		pass
+
