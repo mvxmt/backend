dee59d165a3b0630b0db8430ab3ccc97150379a5
Treelar <me@treelar.xyz>
2025-03-03 23:44:45 -0800

    feat: chat history

--- db/users.py
+++ db/users.py
@@ -22,12 +22,12 @@ async def get_user_by_email(
 ) -> Optional[UserDBO]:
     async with conn.cursor() as cur:
         await cur.execute(
-            "SELECT (name, email, password_hash) FROM user_data.users WHERE email = %s",
+            "SELECT (id, name, email, password_hash) FROM user_data.users WHERE email = %s",
             (email,),
         )
-        user = await cur.fetchone()
+        (user,) = await cur.fetchone()
 
         if user:
-            return UserDBO(name=user[0][0], email=user[0][1], password_hash=user[0][2])
+            return UserDBO(id=user[0], name=user[1], email=user[2], password_hash=user[3])
         else:
             return None
