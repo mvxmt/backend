ea7f901226e95aa93380a95ac65b3b186053f712
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-03-03 18:21:45 -0800

    chore:Added outline for upload manager fixes

--- services/upload.py
+++ services/upload.py
@@ -1,12 +1,14 @@
 import os
+import pathlib
 
 from psycopg import AsyncConnection
-from db.database_documents import DatabaseDocumentManager
+
 from db.database_chunks import DatabaseChunkManager
+from db.database_documents import DatabaseDocumentManager
+
 from .crypto import CryptographyManager
 from .embedding import EmbedManager
 from .parser import Parser
-import pathlib
 
 
 class UploadManager:
@@ -27,6 +29,8 @@ class UploadManager:
         self.em = embed_manager
         self.path = os.getenv("DATA_DIR")
 
+    # Docuemnts no longer needed to be saved to disk
+    # local copy gets ingested and then deleted.
     async def save_to_disk(self, document, filepath: str):
         _, extension = os.path.splitext(filepath)
         match extension[1:]:
@@ -40,12 +44,14 @@ class UploadManager:
                 with open(filepath, "w", encoding="utf-8") as file:
                     file.write(document)
 
+    # Documents no longer need to be uploaded,
+    # they need to be fed into the parser manager
     async def insert_uploaded_document(self, src_file: str, user_id: int):
         data_dir = pathlib.Path(self.path)
         p = data_dir / src_file
 
         if p.exists() and p.is_file():
-            self.dm.insert_document(user_id, src_file)
+            await self.dm.insert_document(user_id, src_file)
 
     async def chunk_uploaded_document(self, filename: str, chunk_size: int):
         filepath = str(self.path + filename)
@@ -61,12 +67,20 @@ class UploadManager:
                 document_id, chunk_ciphertext.decode(), chunk_vector
             )
 
+    # Documents should still be assigned an ID to keep track of
+    # the source document and organize chunks
     async def get_document_id(self, filename: str):
         # Get Inserted Documents ID
         doc = await self.dm.get_document_by_filename(filename)
         return doc[0]
 
+    # Documents no longer have a filepath to retrieve
     async def get_document_path(self, id: int):
         # Get Inserted Documents ID
         doc = await self.dm.get_document_by_id(id)
         return doc[2]
+
+    # Main Driver Function, should route what needs to occur
+    # on document upload.
+    async def on_upload(self):
+        pass
